0fa053bfa423c21942c248d2a5faac052fdcbd84
dariusuknuis <darius_uknuis@yahoo.com>
2026-08-13 21:16:37 -0400

    Fix display of actordef and worlddef panels

--- ui/panel/worlddef.py
+++ ui/panel/worlddef.py
@@ -3,107 +3,74 @@
 import bpy
 from bpy.props import BoolProperty, IntProperty, PointerProperty
 
+
 class QuailWorldDefProperties(bpy.types.PropertyGroup):
+	newworld: BoolProperty(name="New World", default=False)
+	zone: BoolProperty(name="Zone", default=False)
+	eqgzoneversion: IntProperty(name="EQG Zone Version", default=0)
+	use_eqg: BoolProperty(name="EQG", default=False)
+	eqgversion: IntProperty(name="EQG Version", default=0)
 
-    newworld: BoolProperty(
-        name="New World",
-        default=False
-    )
-
-    zone: BoolProperty(
-        name="Zone",
-        default=False
-    )
-
-    eqgzoneversion: IntProperty(
-        name="EQG Zone Version",
-        default=0
-    )
-
-    use_eqg: BoolProperty(
-        name="EQG",
-        default=False
-    )
-
-    eqgversion: IntProperty(
-        name="EQG Version",
-        default=0
-    )
 
 class QUAIL_OT_make_worlddef(bpy.types.Operator):
-    bl_idname = "quail.make_worlddef"
-    bl_label = "Make WORLDDEF"
+	bl_idname = "quail.make_worlddef"
+	bl_label = "Make WORLDDEF"
+	bl_options = {'REGISTER', 'UNDO'}
 
-    bl_options = {'REGISTER', 'UNDO'}
+	@classmethod
+	def poll(cls, context):
+		col = context.collection
+		return bool(col and not col.get("quaildef"))
 
-    @classmethod
-    def poll(cls, context):
+	def execute(self, context):
+		col = context.collection
+		col["quaildef"] = "worlddef"
+		return {'FINISHED'}
 
-        col = context.collection
-
-        if not col:
-            return False
-
-        return not col.get("quaildef")
-
-    def execute(self, context):
-
-        col = context.collection
-
-        col["quaildef"] = "worlddef"
-
-        return {'FINISHED'}
 
 class QUAIL_PT_worlddef_collection(bpy.types.Panel):
-    bl_label = "WORLDDEF"
-    bl_idname = "QUAIL_PT_worlddef_collection"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = "collection"
+	bl_label = "WORLDDEF"
+	bl_idname = "QUAIL_PT_worlddef_collection"
+	bl_space_type = 'PROPERTIES'
+	bl_region_type = 'WINDOW'
+	bl_context = "collection"
 
-    def draw(self, context):
-        layout = self.layout
-        col = context.collection
-        if not col:
-            return
+	@classmethod
+	def poll(cls, context):
+		col = context.collection
+		if not col:
+			return False
 
-        if not col.get("quaildef"):
-            layout.operator(
-                "quail.make_worlddef",
-                icon='WORLD'
-            )
+		quaildef = col.get("quaildef")
+		return quaildef == "worlddef" or quaildef is None
 
-            return
+	def draw(self, context):
+		layout = self.layout
+		col = context.collection
 
-        # WORLDDEF panel
-        if col.get("quaildef") != "worlddef":
-            return
+		if not col.get("quaildef"):
+			layout.operator("quail.make_worlddef", icon='WORLD')
+			return
 
-        props = col.quail_worlddef
-        box = layout.box()
-        box.label(text="WORLDDEF")
-        box.prop(props, "newworld")
-        box.prop(props, "zone")
-        if props.zone and props.use_eqg:
-            box.prop(props, "eqgzoneversion")
+		props = col.quail_worlddef
+		box = layout.box()
+		box.label(text="WORLDDEF")
+		box.prop(props, "newworld")
+		box.prop(props, "zone")
+		box.prop(props, "use_eqg")
 
-        box.prop(props, "use_eqg")
-        if props.use_eqg:
-            box.prop(props, "eqgversion")
+		if props.zone and props.use_eqg:
+			box.prop(props, "eqgzoneversion")
 
+		if props.use_eqg:
+			box.prop(props, "eqgversion")
 
-
-
-# =========================================================
-# REGISTER
-# =========================================================
 
 def register():
+	bpy.types.Collection.quail_worlddef = PointerProperty(
+		type=QuailWorldDefProperties
+	)
 
-    bpy.types.Collection.quail_worlddef = PointerProperty(
-        type=QuailWorldDefProperties
-    )
 
 def unregister():
-
-    del bpy.types.Collection.quail_worlddef
+	del bpy.types.Collection.quail_worlddef
